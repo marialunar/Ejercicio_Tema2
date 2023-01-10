b9278a8fda388be2819a1fa0a6d80f7775896a7e
marialunar <marialunar@gmail.com>
2023-01-10 16:32:07 -0400

    se agrega el proyecto

---
+++ Ejercicio_Tema2.js
@@ -0,0 +1,11 @@
+const lista= ["Maria Alejandra Lunar",
+44, true, new Date("june 18 1978"),
+{
+    titulolib: "Orgullo y Prejuicio",
+    autorlib: "Jane Austen",
+    fechalib: new Date(1913, 1, 28),
+    url_lib: "https://www.elejandria.com/libro/orgullo-y-prejuicio/jane-austen/20"
+
+}
+] 
+console.log(lista)
